8c3471c67d7275199e570826b98d04c5326fd833
Adithyan Panchamoorthy <adithyan3@gatech.edu>
2024-02-28 19:24:02 -0500

    about page instagram link

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,7 +7,6 @@ import Footer from "./Footer";
 import Grid from "@mui/material/Grid";
 import { link } from "fs";
 
-
 const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
@@ -15,22 +14,22 @@ const useStyles = makeStyles(() => ({
     width: "100%",
     // height: '100vh',
     alignItems: "center",
-    overflow:"hidden",
+    overflow: "hidden",
   },
 
   assetContainer: {
     height: "100vh",
     // border: "1px solid black",
     textAlign: "right",
-    width: '100%',
+    width: "100%",
     padding: 0,
   },
 
   textContainer: {
     columnGap: "20px",
     textAlign: "right",
-    paddingLeft:"20px",
-    paddingRight:"20px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
     // height: "40rem",
     // width: "40rem",
     // position: "fixed",
@@ -46,88 +45,114 @@ export const AboutPage = (props) => {
 
   return (
     <div className={classes.root}>
-      <div className={classes.assetContainer} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <div
+        className={classes.assetContainer}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <InfoRender />
       </div>
 
       <Grid
-          container
-          className={classes.textContainer} 
-          style={{
-            width: largeScreen ? '100%' : '100%', 
-            padding: largeScreen ? '0px 10px 0px 10px' : '0px 4px 0px 4px', 
-            // boxSizing: 'border-box', bottom: largeScreen ? '0rem' : "16px", 
-            flexWrap:"nowrap",
-            alignItems:"flex-end",
-            position: "fixed",
-            bottom: "0",
-            paddingBottom:"10px",
-            justifyContent: "flex-end",
+        container
+        className={classes.textContainer}
+        style={{
+          width: largeScreen ? "100%" : "100%",
+          padding: largeScreen ? "0px 10px 0px 10px" : "0px 4px 0px 4px",
+          // boxSizing: 'border-box', bottom: largeScreen ? '0rem' : "16px",
+          flexWrap: "nowrap",
+          alignItems: "flex-end",
+          position: "fixed",
+          bottom: "0",
+          paddingBottom: "10px",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Grid
+          item
+          xs={6}
+          sm={12}
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            flexwrap: "nowrap",
           }}
         >
-          <Grid
-            item
-            xs={6}
-            sm={12}
-            sx={{ display: "flex", justifyContent: "flex-start" , flexwrap:"nowrap",}}
-          >
-            <Typography
+          <Typography
             style={{
               fontFamily: "Square721",
               fontSize: largeScreen ? ".7rem" : ".55rem",
               // right: "0",
               // paddingBottom:"10px",
-              textAlign:"left",
-             }}>
-            Pleasure Craft is a NYC-based creative studio and directing duo that specializes in CGI and live-action cinematography to design visual worlds that transcend the ordinary.
-            </Typography>
-          </Grid>
+              textAlign: "left",
+            }}
+          >
+            Pleasure Craft is a NYC-based creative studio and directing duo that
+            specializes in CGI and live-action cinematography to design visual
+            worlds that transcend the ordinary.
+          </Typography>
+        </Grid>
 
-          <Grid
-            item
-            xs={6}
-            sm={12}
-            sx={{ display: "flex", justifyContent: "flex-end" , flexwrap:"nowrap",}}
+        <Grid
+          item
+          xs={6}
+          sm={12}
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flexwrap: "nowrap",
+          }}
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <Typography
             style={{
+              fontFamily: "Square721",
+              fontSize: largeScreen ? ".7rem" : ".55rem",
+              // right: "0",
+              // paddingBottom:"10px",
+              textTransform: "Uppercase",
               textDecoration: "none",
             }}
           >
-            <Typography
-            style={{
-              fontFamily: "Square721",
-              fontSize: largeScreen ? ".7rem" : ".55rem",
-              // right: "0",
-              // paddingBottom:"10px",
-              textTransform:"Uppercase",
-              textDecoration: "none",
-             }}>
             info@pleasurecraft.fun
-            </Typography>
-          </Grid>
+          </Typography>
+        </Grid>
 
-          <Grid
-            item
-            xs={6}
-            sm={12}
-            sx={{ display: "flex", justifyContent: "flex-end" , flexwrap:"nowrap",}}
-            style={{
-            }}
+        <Grid
+          item
+          xs={6}
+          sm={12}
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flexwrap: "nowrap",
+          }}
+          style={{}}
+        >
+          <a
+            style={{ color: "#000000", textDecoration: "none" }}
+            href="https://www.instagram.com/pleasurecraft.fun"
+            rel="noreferrer"
           >
             <Typography
-            style={{
-              fontFamily: "Square721",
-              fontSize: largeScreen ? ".7rem" : ".55rem",
-              textTransform:"Uppercase",
-              // right: "0",
-              // paddingBottom:"10px",
-             }}>
-            Instagram
+              style={{
+                fontFamily: "Square721",
+                fontSize: largeScreen ? ".7rem" : ".55rem",
+                textTransform: "Uppercase",
+                // right: "0",
+                // paddingBottom:"10px",
+              }}
+            >
+              INSTAGRAM
             </Typography>
-          </Grid>
-
+          </a>
+        </Grid>
       </Grid>
-
-
     </div>
   );
 };
